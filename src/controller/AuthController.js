209e5e3bba8aa092bd89715e44f0632d2f1097ce
Adam Almeida <adamportifolio@gmail.com>
2022-09-06 21:36:07 -0300

    Create Signup User

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,4 +1,9 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const { validationResult, matchedData } = require('express-validator')
+
+const User = require('../models/User')
+const State = require('../models/State')
 
 module.exports = {
     signin: async (req, res) => {
@@ -13,6 +18,43 @@ module.exports = {
         }
 
         const data = matchedData(req)
-        res.json({tudocerto:true, data})
+        const user = await User.findOne({
+            email: data.email
+        })
+
+        if (user) {
+            res.json({
+                error: { email: { msg: 'Usuário já cadastrado com este email.' } }
+            })
+            return
+        }
+        if (mongoose.Types.ObjectId.isValid(data.state)) {
+            const state = await State.findById(data.state)
+            if (!state) {
+                res.json({
+                    error: { state: { msg: 'O estado não parece válido.' } }
+                })
+                return
+            }
+        } else {
+            res.json({
+                error: { state: { msg: 'Código do estado inválido.' } }
+            })
+            return
+        }
+
+        const passwordHash = await bcrypt.hash(data.password, 10)
+        const payload = (Date.now() + Math.random()).toString()
+        const token = await bcrypt.hash(payload, 10)
+
+        const newUser = new User({
+            name: data.name,
+            email: data.email,
+            passwordHash: passwordHash,
+            token,
+            state: data.state
+        })
+        await newUser.save()
+        res.json({ token })
     }
 }
