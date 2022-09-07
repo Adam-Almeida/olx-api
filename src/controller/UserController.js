415b7820004b259aacc63125e91a5be0d11cec07
Adam Almeida <adamportifolio@gmail.com>
2022-09-07 00:15:14 -0300

    List info User

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State')
+const User = require('../models/User')
+const Category = require('../models/Category')
+const Ad = require('../models/Ad')
 
 module.exports = {
     getStates: async (req, res) => {
@@ -7,7 +10,25 @@ module.exports = {
     },
 
     info: async (req, res) => {
+        let { token } = req.query;
+        const user = await User.findOne({token})
+        const state = await State.findById(user.state)
+        const ads = await Ad.find({idUser: user._id.toString()})
 
+        let adsList = []
+        for(let i in ads){
+            const category = await Category.findById(ads[i].category)
+            adsList.push({
+                ...ads[i], category.slug
+            })
+        }
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            state: state.name,
+            ads: adsList
+        })
     },
 
     editAction: async (req, res) => {
