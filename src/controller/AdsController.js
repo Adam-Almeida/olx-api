71b23a15d798ab34e10d671965bca214fc99c4fe
adam-almeida <adamportifolio@gmail.com>
2022-09-14 22:14:22 -0300

    Listando categorias

--- src/controller/AdsController.js
+++ src/controller/AdsController.js
@@ -1,6 +1,19 @@
+const Category = require("../models/Category")
+
 module.exports = {
     getCategories: async (req, res) => {
+        const categoryAll = await Category.find()
 
+        let categories = []
+
+        for(let i in categoryAll) {
+            categories.push({
+                ...categoryAll[i]._doc,
+                img: `${process.env.BASE}/assets/images/${categoryAll[i].slug}.png`
+            })
+        }
+
+        res.json({categories})
     },
     addAction: async (req, res) => {
 
