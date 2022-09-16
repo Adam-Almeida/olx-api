0285f3fc5994ce347f87a244409b8cb22a29018e
adam-almeida <adamportifolio@gmail.com>
2022-09-15 22:32:30 -0300

    Controller para adicionar anuncio

--- src/controller/AdsController.js
+++ src/controller/AdsController.js
@@ -1,4 +1,6 @@
+const Ad = require("../models/Ad")
 const Category = require("../models/Category")
+const User = require("../models/User")
 
 module.exports = {
     getCategories: async (req, res) => {
@@ -15,7 +17,34 @@ module.exports = {
 
         res.json({categories})
     },
+
     addAction: async (req, res) => {
+        let { title, price, priceNegotiable, description, category, token} = req.body
+        const user = await User.findOne({token}).exec()
+
+        if(!title || !category){
+            res.json({error: "Título e categoria são obrigatórios"})
+            return
+        }
+
+        if(price){
+            price = price.replace('.', '').replace(',', '.').replace('R$ ', '')
+            price = parseFloat(price)
+        }else{
+            price = 0
+        }
+
+        const newAdd = new Ad()
+        newAdd.status = true
+        newAdd.idUser = user._id
+        newAdd.state = user.state
+        newAdd.dateCreated = new Date()
+        newAdd.title = title
+        newAdd.category = category
+        newAdd.price = price
+        newAdd.priceNegotiable = (priceNegotiable === 'true' || priceNegotiable === 'on' ? true : false)
+        newAdd.description = description
+        newAdd.views = 0
 
     },
     getList: async (req, res) => {
