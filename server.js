24c202acaa20f1c3ccd02b542f9aa3006dd28e9a
adam-almeida <adamportifolio@gmail.com>
2022-09-17 13:04:34 -0300

    Iniciando a documentação com Swagger Ui

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const routes = require('./src/routes')
+const swaggerFile = require('./swagger.json')
 
 const server = express()
 
@@ -18,6 +20,7 @@ server.use(express.urlencoded({
 server.use(fileUpload())
 server.use(express.static(__dirname + '/public'))
 
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 server.use("/", routes)
 
 server.listen(process.env.PORT, () => {
